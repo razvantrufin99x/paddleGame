5769781879da15e849ba7f79a46dba4c1aef542f
RAZVAN TRUFIN <razvantrufin99x@gmail.com>
2022-11-27 02:46:33 +0200

    Add project files.

---
+++ paddleGame/Form1.Designer.cs
@@ -0,0 +1,86 @@
+﻿namespace paddleGame
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.GraphicsWindow = new graphicswithpermanentcontrol.UserControl1();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // GraphicsWindow
+            // 
+            this.GraphicsWindow.BackColor = System.Drawing.Color.White;
+            this.GraphicsWindow.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.GraphicsWindow.Cursor = System.Windows.Forms.Cursors.Default;
+            this.GraphicsWindow.Location = new System.Drawing.Point(12, 34);
+            this.GraphicsWindow.Name = "GraphicsWindow";
+            this.GraphicsWindow.Size = new System.Drawing.Size(400, 226);
+            this.GraphicsWindow.TabIndex = 0;
+            this.GraphicsWindow.MouseMove += new System.Windows.Forms.MouseEventHandler(this.GraphicsWindow_MouseMove);
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 1;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 5);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(400, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "double click in the image below and then press this button to start the game";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(418, 273);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.GraphicsWindow);
+            this.Name = "Form1";
+            this.Text = "Paddle SmallBasic translated into c net";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private graphicswithpermanentcontrol.UserControl1 GraphicsWindow;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Button button1;
+
+    }
+}
+
---
+++ paddleGame/Form1.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Threading;
+
+namespace paddleGame
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+        Rectangle paddle = new Rectangle();
+        Rectangle ball = new Rectangle();
+        int x = 0;
+        int y = 0;
+        int deltaX = 1;
+        int deltaY = 2;
+        int score = 0;
+        int mousex = 0;
+        int mousey = 0;
+        int gw;
+        int gh;
+
+
+        public int game()
+        {
+            GraphicsWindow.g.Clear(Color.White);
+            PrintScore(score.ToString());
+
+
+
+            x = x + deltaX;
+            y = y + deltaY;
+
+
+            if (x >= gw - 16 || x <= 0)
+            {
+                deltaX = -deltaX;
+            }
+            if (y <= 0)
+            {
+                deltaY = -deltaY;
+            }
+
+            int padX = paddle.Left;
+
+            if ((y == gh - 20) && (x >= padX) && (x <= padX + 60))
+            {
+               
+                score = score + 10;
+                PrintScore(score.ToString());
+                deltaY = -deltaY;
+                
+            }
+
+            ball.X = x;
+            ball.Y = y;
+
+
+            GraphicsWindow.g.DrawRectangle(GraphicsWindow.p, x, y, 10, 10);
+            GraphicsWindow.g.DrawRectangle(GraphicsWindow.p, mousex, GraphicsWindow.Height - 25, 50, 18);
+
+
+          
+
+
+            if (y < gh)
+            {
+                return 1;
+            }
+            return 0;
+        }
+
+        public void PrintScore(string s)
+        {
+            GraphicsWindow.g.DrawRectangle(GraphicsWindow.p, 10, 10, 200, 20);
+            GraphicsWindow.g.DrawString("Score: " + s, GraphicsWindow.f, GraphicsWindow.b, 10, 10);
+            GraphicsWindow.g.DrawString(x.ToString() + " : " + y.ToString(), GraphicsWindow.f, GraphicsWindow.b, 260, 10);
+            GraphicsWindow.g.DrawString(mousex.ToString() + " : " + mousey.ToString(), GraphicsWindow.f, GraphicsWindow.b, 320, 10);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            timer1.Enabled = false;
+
+            gw = GraphicsWindow.Width;
+            gh = GraphicsWindow.Height - 20;
+
+        }
+
+        private void GraphicsWindow_MouseMove(object sender, MouseEventArgs e)
+        {
+            mousex = e.X;
+            mousey = e.Y;
+            paddle.X = mousex - 15;
+            paddle.Y = mousey;
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            game();
+            GraphicsWindow.Refresh();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            timer1.Enabled = true;
+
+        }
+    }
+}
